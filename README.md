e4283bc944850156070d843286994bd3988978e9
Chris West <vpht64@zebra.com>
2015-11-20 15:00:15 -0500

    Added more descriptive README support. Added message object on set actions and subscriber callbacks

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # DataStore
-Cross web component data store and onChange subscriptions. This lightweight solution allows cross component access to data mutations events, as well as added support for cached data models when the user is offline.
+Cross web component data store and onChange subscriptions. This lightweight solution allows cross component access to
+data mutations events, as well as added support for cached data models when the user is offline.
 
 ```sh
 # downloading all dependencies
@@ -12,4 +13,65 @@ Then check out : http://localhost:3002/
 
 TODO: More detailed information to follow
 
+## Methods
 
+### setStore (String [name], Object [data], Object [Options])
+Allows the user to set a data object to the DataStore by specified key, with added options for subscription management.
+Once the data object is registered and cached, it will then fire the collection of subscribed callbacks.
+
+Options:
+persist - boolean | Toggle to allow LocalStorage caching when setting data.
+message - String | Will broadcast this message to all subscriber callbacks for specific consumer handling.
+
+```
+var obj = {
+    name: 'Chris',
+    color: 'Blue'
+};
+
+Store.setStore(
+    'myObject',
+    obj,
+    {
+        persist: true,
+        message: 'updating'
+    }
+);
+
+```
+
+### getStore (String [name])
+Getting the current data object out of the DataStore. In cases where the data is set to persist, this will get the
+cached data model on first hit form the local cache. No setting necessary.
+```
+var obj = {
+    name: 'Chris',
+    color: 'Blue'
+};
+
+var myData = Store.getStore('myObject');
+// {
+//       name: 'Chris',
+//       color: 'Blue'
+// }
+
+```
+
+### subscribe (String [name], Function [callback])
+Attaches a handler to fire callbacks when the data is updated in the DataStore. The data and message objects will be
+passed through to the callback.
+
+```
+var myCallbackFn = function(data, msg) {
+    console.log(data, msg);
+}
+
+Store.subscribe('myObject', myCallbackFn);
+
+// When setStore is executed, the following will be outputted from the callback function:
+// {
+//       name: 'Chris',
+//       color: 'Blue'
+// }, updated
+
+```
--- src/app/models/Store.js
+++ src/app/models/Store.js
@@ -1,5 +1,7 @@
-
-
+/*
+ DataStore | Chris West inkorange.com
+ Cross component data management container with tie ins to Local Storage persistence
+ */
 
 module.exports = {
     _initStore: function (name) {
@@ -9,6 +11,7 @@ module.exports = {
         if(!window.store[name]) {
             window.store[name] = {};
             window.store[name].data = null;
+            window.store[name].message = null;
             window.store[name].subscriptions = [];
         }
     },
@@ -22,19 +25,26 @@ module.exports = {
         // initializes if not already done
         this._initStore(name);
 
+        // sets the data to the store object
         window.store[name].data = data;
+
+        // when a message is set, it would set it to the store data object
+        window.store[name].message = options.message ? options.message : '';
+
+        // if the setter contains a persist config, it will add it to local storage
         if(options.persist) {
             // save to LocalStorage
             localStorage.setItem(name, JSON.stringify(window.store[name].data));
         }
+
         var subscriptions = window.store[name].subscriptions ? window.store[name].subscriptions : [];
         if(subscriptions.length > 0) {
             subscriptions.forEach(function (fn) {
-                fn(data); // executing each callback that is subscribed
+                fn(data, window.store[name].message); // executing each callback that is subscribed
             });
         }
     },
-    
+
     getStore: function(name) {
         var data = {};
 
@@ -43,7 +53,6 @@ module.exports = {
 
         if(!window.store[name].data) {
             var CachedData = JSON.parse(localStorage.getItem(name));
-            console.log('getting '+name+' form localStorage: ', CachedData);
             window.store[name].data = CachedData;
             data = window.store[name].data;
         } else {
@@ -51,14 +60,14 @@ module.exports = {
         }
         return data;
     },
-    
+
     subscribe: function(name, callbackFn) {
         this._initStore(name);
         window.store[name].subscriptions.push(callbackFn); // subscribing to this store.
     },
-    
+
     getSubscribers: function(name) {
         return window.store[name].subscriptions;
     }
-    
+
 };
